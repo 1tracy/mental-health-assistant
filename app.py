c0da2caf912cd8362cc555bd2348bced18327a17
tracy! <55264469+1tracy@users.noreply.github.com>
2021-08-04 10:56:13 -0400

    add /api/ extension

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 from flask.wrappers import Request
 from flask_cors import CORS
+from werkzeug.utils import send_from_directory
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/hello')
+@app.route('/api/hello')
 def say_hello_world():
     return {'result': "Hello World"}
 
-@app.route('/login', methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def login(): 
     print(request)
     content = request.json
