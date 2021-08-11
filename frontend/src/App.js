d6a294c3792aa32a6cd295620cf182323f31706c
Angelyn Domingo <angelyn.domingo122@gmail.com>
2021-08-11 11:12:10 -0400

    Make tab pretty~

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,23 +8,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
 	return (
 		<div className="app">
-			<body class="homepage is-preload">
-				<div id="page-wrapper">
-					<section id="header">
-						<div class="container">
-							<nav id="nav">
-								<Router>
-									<Nav />
-										<Switch>
-											<Route exact path="/resources" component={Help} />
-											<Route path="/" exact component={Home} />
-										</Switch>
-								</Router>
-							</nav>
-						</div>
-					</section>
-				</div>
-			</body>
+			<div id="page-wrapper">
+				<section id="header">
+					<div class="container">
+						<nav id="nav">
+							<Router>
+								<Nav />
+									<Switch>
+										<Route exact path="/resources" component={Help} />
+										<Route path="/" exact component={Home} />
+									</Switch>
+							</Router>
+						</nav>
+					</div>
+				</section>
+			</div>
 		</div>
 	);
 }
